3ad87b1345ea435e12e63b399ccc319a21f2cbc0
FranchescaDelaRosaFDGDR <delarosa.fg@obmontessori.edu.ph>
2026-09-09 10:40:58 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# Receipt Generator 
+from pyscript import display, document
+
+def getting_total(e):
+    document.getElementById('result').innerHTML = ""
+    
+    selected_coffee = document.querySelector(".menu-item:checked")
+    
+    subtotal = float(selected_coffee.value)
+    vat = subtotal * 0.12
+    total = subtotal + vat
+    
+    display(f"Subtotal: Php {subtotal}", target="result")
+    display(f"Tax (12%): Php {vat}", target="result")
+    display(f"Total payment: Php {total}", target="result")
+
